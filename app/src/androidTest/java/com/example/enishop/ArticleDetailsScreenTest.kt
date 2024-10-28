aec55db0f8258692fd999a94a992911924be7e11
naloulou2023 <aloulou.naycen@gmail.com>
2024-10-28 15:43:34 +0100

    init test

---
+++ app/src/androidTest/java/com/example/enishop/ArticleDetailsScreenTest.kt
@@ -0,0 +1,21 @@
+package com.example.enishop
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.enishop.BO.Article
+import com.example.enishop.Dao.memory.ArticleDaoMemoryImpl
+import com.example.enishop.repository.ArticleRepository
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.util.Date
+
+class ArticleDetailsScreenTest() {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testArticleDetailsScreen() {
+
+    }
+
+}
