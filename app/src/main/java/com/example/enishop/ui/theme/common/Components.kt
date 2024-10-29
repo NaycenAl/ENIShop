4ddabef35526996384bb4c60efd38f5d8c944580
naloulou2023 <aloulou.naycen@gmail.com>
2024-10-29 16:54:13 +0100

    Adding a menu button and drop down menu in topBar

--- app/src/main/java/com/example/enishop/ui/theme/common/Components.kt
+++ app/src/main/java/com/example/enishop/ui/theme/common/Components.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,7 +10,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,6 +22,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -33,6 +41,9 @@ import com.example.enishop.ui.theme.common.FAB
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBarShop(onClickToBack: () -> Unit = {}, modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable()
+    { mutableStateOf(false) }
+    val items = listOf("Option 1","Option 2", "Option 3")
     TopAppBar(
 
         title = {
@@ -58,9 +69,25 @@ fun AppBarShop(onClickToBack: () -> Unit = {}, modifier: Modifier = Modifier) {
                 )
                 Spacer(modifier = Modifier.weight(1f))
 
-
+                Box {
+                    IconButton(onClick = { expanded = !expanded }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "menu",
+                            tint = Color(0xFF367DDC)
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        items.forEach { item ->
+                          DropdownMenuItem(text = { Text(text = item) }, onClick = {  })
+                        }
+                    }
+                }
             }
-        },
+            },
         navigationIcon = {
 
 
